74968fa78f4d59019cf00b13b580b7cfabefcef3
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2023-07-19 18:54:44 +1200

    Installed required packages. Forgot to create .env.local file so will grab the url then continue

---
+++ src/pages/api/cardAdd.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+require("../../mongoDB").connect();
+const cardModel = require("../../models/card");
+
+export default async function handler(req, res) {
+  const { name, image } = req.body;
+  const existingCard = await cardModel.findOne({});
+  arrayChecker(keywords);
+  if (req.method !== "POST") {
+    console.log("Method not allowed");
+    res.status(405).redirect("/addcard");
+    res.status(400).redirect("/addcard");
+  } else {
+    const card = await cardModel.create({
+      name,
+      image,
+    });
+    console.log(`Successfully stored "${card.name}" in database`);
+    res.status(200).redirect("/addcard");
+  }
+}
---
+++ src/pages/form/form.js
@@ -0,0 +1,14 @@
+<form action="/api/cardAdd" method="post">
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" required>
+    <br />
+  </input>
+  <label for="image">Image:</label>
+  <input type="text" id="image" name="image" required>
+    <br />
+  </input>
+</form>;
+
+// use testing playground to find correct jest syntax
+// screen.findbyrole('input type equivilent', {name: 'name of input'})
+// user.type() is the event handler for input fields
